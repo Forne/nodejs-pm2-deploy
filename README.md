a01a21a8e18b3f6e75bbc9eab2cb5d3166aaf519
Dmitriy Pervin <theforner@gmail.com>
2024-09-28 01:45:44 +0500

    Create ecosystem.config.js

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Деплой приложения на сервер с использованием pm2
 
-Стартеркит проекта по автоматизации деплоя фронтенда и бэкенда при помощи pm2 (pm2 deploy)
+* IP адрес praktikum@89.169.171.137
+* Frontend https://mesto.dp.nomorepartiesco.ru
+* Backend https://api.mesto.dp.nomorepartiesco.ru
---
+++ frontend/ecosystem.config.js
@@ -0,0 +1,26 @@
+require('dotenv').config({ path: '.env.deploy' });
+
+const {
+  DEPLOY_USER,
+  DEPLOY_HOST,
+  DEPLOY_PATH,
+  DEPLOY_REPO,
+  DEPLOY_REF = 'origin/feat-deploy',
+} = process.env;
+
+module.exports = {
+  apps: [{
+    name: 'mesto-frontend',
+    script: './dist/app.js',
+  }],
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: DEPLOY_REPO,
+      path: DEPLOY_PATH,
+      'post-deploy': 'cd frontend && npm i && npm run build',
+    },
+  },
+};
